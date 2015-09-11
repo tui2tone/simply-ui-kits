fce7e3a1d05d3bdd7bd57684fe1b045ec481f28a
Charuwit Nodthaisong <tui2tone@Charuwits-MacBook-Air.local>
2015-09-12 02:27:10 +0700

    Add Chart.js

--- tasks/html.js
+++ tasks/html.js
@@ -7,7 +7,7 @@ gulp.task('html', function() {
   gulp.src(['./node_modules/jquery/**/*',]).pipe(gulp.dest('./public/lib/jquery'))
   gulp.src(['./node_modules/bootstrap/**/*',]).pipe(gulp.dest('./public/lib/bootstrap'))
   gulp.src(['./node_modules/font-awesome/**/*',]).pipe(gulp.dest('./public/lib/font-awesome'))
-
+  gulp.src(['./node_modules/chartist/**/*',]).pipe(gulp.dest('./public/lib/chartist'))
 
   return gulp.src(['./app/**/*.html', '!./app/include/*.html'])
     .pipe(fileinclude({
